b36df3d32b50a17b2937917c84ed9b11cd772dc6
gustavolopesv3 <gustavo.lopesv3@gmail.com>
2019-09-04 12:36:13 -0500

    extend de navbar do base e deixado cabeçado independente para cada tela (index e about) e criado url index para botão home.

--- website/urls.py
+++ website/urls.py
@@ -5,6 +5,7 @@ from blog import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name = 'index'),
+    path('index/', views.index, name = 'index'),
     path('post/<int:id>/', views.post, name = 'post'),
     path('about/',views.about, name = 'about'),
     path('contact/',views.contact, name = 'contact')
